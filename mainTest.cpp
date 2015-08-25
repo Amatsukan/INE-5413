70f216fe7be2038fdc56c2aedb7112c993f8e52f
Luiz LuLz <luiz.cfj@apilab.ufsc.br>
2015-08-24 21:19:23 -0300

    Update mainTest.cpp

--- mainTest.cpp
+++ mainTest.cpp
@@ -7,13 +7,5 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    Vertex v = Vertex(999);
-    Digraph d = Digraph("test");
-    d.addVertex(v);
-    d.addVertex(50);
-    cout<<d.contains(50)<<endl;
-    cout<<d.contains(v)<<endl;
-    d.connect(v,50);
-    cout<<d.degree_of_issuance(50)<<endl;
-    cout<<d.degree_of_reception(50)<<endl;
+
 }
